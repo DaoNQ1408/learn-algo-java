40fda09263184bb73a02569a0b53ae11cb80d5d2
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-01-29 16:28:48 +0700

    adding comment

--- LinkedList/RemoveDuplicatesFromSortedList.java
+++ LinkedList/RemoveDuplicatesFromSortedList.java
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        // dùng curr và nextt để check value = nhau hay không ?
         ListNode curr = head;
 
         if(curr == null || curr.next == null) {
@@ -10,18 +11,18 @@ class Solution {
         ListNode nextt = curr.next;
 
         do {
-            if(nextt.val == curr.val) {
-                if(nextt.next == null) {
+            if(nextt.val == curr.val) { // value = nhau
+                if(nextt.next == null) { // nextt là node cuối cùng
                     curr.next = null;
-                } else {
+                } else { // nextt không phải là node cuối cùng
                     curr.next = nextt.next;
                     nextt = curr.next;
                 }
-            } else {
+            } else { // value != nhau thì duyệt tiếp các node tiếp theo
                 nextt = nextt.next;
                 curr = curr.next;
             }
-        } while(curr.next != null);
+        } while(curr.next != null); // khi nào curr.next == null thì dừng
 
         return head;
     }
